3794b707c2ec1a1f755ff930915c58b8cf8a2bbb
qkrdlstj147 <gtavictsaven147@gmail.com>
2023-11-27 11:25:04 +0900

    V1

---
+++ week14/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	vote = votes.txt
+	lines, err := vote.txt.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	counts := make(map[string]int)
+	for _, line := range lines {
+		counts[line]++
+	}
+	for name, count := range counts {
+		fmt.Printf("Votes for %s: %d\n", name, count)
+	}
+}
